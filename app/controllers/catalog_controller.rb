38e5f81075476b35c9a3ac9c7bd6c54b0d6e2d39
Zaker Siraj <zaker.siraj@gmail.com>
2016-12-21 22:56:15 -0500

    Added record title field.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -55,12 +55,12 @@ class CatalogController < ApplicationController
     }
 
     # solr field configuration for search results/index views
-    config.index.title_field = 'title_display'
+    config.index.title_field = 'title'
     config.index.display_type_field = 'format'
 
     # solr field configuration for document/show views
-    #config.show.title_field = 'title_display'
-    #config.show.display_type_field = 'format'
+    config.show.title_field = 'title'
+    config.show.display_type_field = 'format'
 
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
